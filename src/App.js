17df8ff78222605c88b86fa37b236ceff3b40eea
Tomikimi Ogoli <tomikimiogoli@yahoo.com>
2020-11-26 22:11:25 +0100

    modified app.js and publc folder index html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+import Home from "./pages/HomePage/Home";
+import Main from "./pages/MainPage/Main";
+import notFound from "./pages/notFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle></GlobalStyle>
+      <Switch>
+        <Route path="/Main/:name/:education" component={Main}></Route>
+        <Route path="/Main/:name" component={Main}></Route>
+        <Route path="/not-found" component={notFound}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Redirect to="/not-found" />
+      </Switch>
+    </Router>
   );
 }
 
